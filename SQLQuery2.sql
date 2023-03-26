c7aaf055f230499def916621a69fe75bce7895ab
AartiPundalik <aartipundalik68@gmail.com>
2023-03-26 17:15:27 +0530

    [Aarti] Add : UC2 - Creating Table For Employee Pay Roll Data Base

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE EmployeePayRoll_DB
+
+USE EmployeePayRoll_DB
+
+-- UC2 
+CREATE TABLE EmployeePayRoll_Table
+(
+EmployeeId INT IDENTITY(1,1) PRIMARY KEY,
+EmployeeName varchar(225),
+EmployeeSalary INT,
+StartDate Date
+);
